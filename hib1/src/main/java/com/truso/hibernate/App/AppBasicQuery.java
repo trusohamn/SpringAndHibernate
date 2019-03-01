37c0689fa1f4bab759e21030ec0a380053bc89a3
Marta Trusohamn <marta.trusohamn@gmail.com>
2019-03-01 17:04:57 +0100

    hib query

---
+++ hib1/src/main/java/com/truso/hibernate/App/AppBasicQuery.java
@@ -0,0 +1,45 @@
+package com.truso.hibernate.App;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.truso.hibernate.entity.Person;
+
+public class AppBasicQuery {
+	
+	public static void main(String[] args) {
+		//session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Person.class)
+				.buildSessionFactory();
+				
+		//session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//QUERY 
+			
+			session.beginTransaction();
+			
+			System.out.println("all persons:");
+			List<Person> myList = session.createQuery("from Person").getResultList();	
+			myList.forEach(e -> System.out.println(e));
+		
+			System.out.println("\n person named Marta: ");
+			myList = session.createQuery("from Person p where p.firstName = 'Marta'").getResultList();	
+			myList.forEach(e -> System.out.println(e));
+			
+			session.getTransaction().commit();
+
+			
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+}
